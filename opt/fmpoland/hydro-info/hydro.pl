8e563d0af52f13d9f2447ac9c47fc6b0ec176a75
Waldek <sp2ong@gmail.com>
2023-06-13 13:34:34 +0200

    update hydro-info

--- opt/fmpoland/hydro-info/hydro.pl
+++ opt/fmpoland/hydro-info/hydro.pl
@@ -1,21 +1,24 @@
 #!/usr/bin/perl
+#
 # SP2ONG Hydro Info script - poziom rzek
 #  
-#
 #  Wykaz punktow pomiaru: https://hydro.imgw.pl/
 #
+# Potrzebny numer wodowskazu oraz plik wav z numerem wodowskazu
+# gdzie nagranie zawiera nazwę wodowskazu np 154180150.wav
+# Plik numer_wodowskazu.wav umiescic w /usr/share/svxlink/sounds/pl_PL/HydroInfo/
 # 
 
 ########## Configuration 
-# ID wodowskazu
-my $idstn="151170030";
-# Poziom stanu alarmowego
-my $alert="450";
-# poziom stanu ostrzegawczego
-my $warning="380";
-# nazw pliku wav w /usr/share/svxlink/sounds/pl_PL/HydroInfo/
-# ktory ma nagrane info np "Wodowskaz Sobianowice rzeka Bystrzyca"
-my $name_river="odra_trestno";
+
+# Lista ID wodowskazów
+
+# dla jednego wodowskazu:
+my @idstn=(153180090);
+
+# kiedy więcej niż jeden wodowskaz użyj takiego zapisu:
+#my @idstn=(151170030,150170040);
+
 ######################
 
 use strict;
@@ -24,73 +27,80 @@ use JSON qw(decode_json);
 use LWP::Simple qw(get);
 use Data::Dumper;              
 
-# IMGW Hydro API JSON dane
-my $url="https://danepubliczne.imgw.pl/api/data/hydro";
-my $decode = eval {decode_json(get($url))};
-
-#print length($decode);
-
-# Zmienn lokalne dla danych z wodoskazu
-my $sw=0;
-my $Tsw;
 my $Htime;
 my $time="";
 my $h1=0;
 my $m1=0;
-
-
 my $ids;
-my $i=0;
+my $alarm;
+my $current;
+my $warning;
+my $trend;
+my $data=0;
 
-if (-e "/var/spool/svxlink/bulletins/hydro.tcl") { unlink "/var/spool/svxlink/bulletins/hydro.tcl" };
+if (-e "/var/spool/svxlink/bulletins/hydro.tcl") { unlink "/var/spool/svxlink/bulletins/hydro.tcl" }; 
+
+open(WX,">/var/spool/svxlink/bulletins/hydro.tcl");
+print WX "playMsg \"HydroInfo\" \"hydro\";\n";
+print WX "playSilence 200;\n";
+
+for my $i (@idstn) {
+
+my $url="https://hydro.imgw.pl/api/station/hydro/?id=$i";
+
+my $decode = eval {decode_json(get($url))} ;
+
+#print Dumper ($decode);
 
 
 if(!$@){
 
-open(WX,">/var/spool/svxlink/bulletins/hydro.tcl");
+if (length($decode)) {
 
-# pobieranie danych dla wskazane stacji o danym ID
-for ($i=0; $i<609; $i++) {
+$data = 1;
 
- my $id = $decode->[$i]{'id_stacji'};
- $ids = $id+0;
- $sw = $decode->[$i]{'stan_wody'};
- $time = $decode->[$i]{'stan_wody_data_pomiaru'};
+$alarm = $decode->{'status'}{'alarmValue'};
+$warning = $decode->{'status'}{'warningValue'};
+$current = $decode->{'status'}{'currentState'}{'value'};
+$time = $decode->{'status'}{'currentState'}{'date'};
+$trend = $decode->{'trend'};
+$ids = $i;
 
-# ID stacji 151220100 Sobianowice
-if ( $ids eq $idstn) {
- $Tsw=$sw;
- $Htime=substr $time,11,1;
+ $Htime = substr $time,11,1;
  if ($Htime==0) {
   $h1=substr $time,12,1;
-  } else {
-  $h1 = substr $time,11,2;
-  }
-  $m1=substr $time,14,2;
- }
+  } else { $h1 = substr $time,11,2;}
+ $m1=substr $time,14,2;
 
-
-}
-
-# Gnerowanie pliku tcl dla svxlink
-
-  print WX "playMsg \"HydroInfo\" \"hydro\";\n";
-  print WX "playMsg \"HydroInfo\" \"$name_river\";\n";
+  print WX "playMsg \"HydroInfo\" \"$ids\";\n";
   print WX "playMsg \"HydroInfo\" \"godzina_pomiaru\";\n";
   print WX "playTime $h1 $m1;\n";
+  print WX "playSilence 100;\n";
   print WX "playMsg \"HydroInfo\" \"stan_aktualny\";\n";
-  print WX "playNumber $Tsw;\n";
+  print WX "playSilence 100;\n";
+  print WX "playNumber $current;\n";
   print WX "playMsg \"HydroInfo\" \"unit_cm\";\n";
-if ($Tsw>$alert && $Tsw < $warning) {
+  print WX "playSilence 100;\n";
+  print WX "playMsg \"HydroInfo\" \"trend\";\n";
+  print WX "playMsg \"HydroInfo\" \"$trend\";\n";
+
+if ($current>$warning && $current < $alarm) {
+  print WX "playSilence 100;\n";
   print WX "playMsg \"HydroInfo\" \"przekroczony_ostrz\";\n";
  }
-if ($Tsw>=$alert) {
+if ($current>=$alarm) {
+  print WX "playSilence 100;\n";
   print WX "playMsg \"HydroInfo\" \"przekroczony_alarm\";\n";
  }
+  print WX "playSilence 300;\n";
+  #print WX "playMsg \"HydroInfo\" \"hydro_source\";\n";
 
-  print WX "playSilence 200;\n";
-  print WX "playMsg \"HydroInfo\" \"hydro_source\";\n";
-
+  }
+ }
+}
 close(WX);
 
+ if ( $data == 0) {
+   if (-e "/var/spool/svxlink/bulletins/hydro.tcl") { unlink "/var/spool/svxlink/bulletins/hydro.tcl" }; 
 }
+
--- opt/fmpoland/hydro-info/readme.txt
+++ opt/fmpoland/hydro-info/readme.txt
@@ -4,24 +4,28 @@ Instalacje pakietu
 sudo apt install -y libjson-perl libwww-perl
 
 Jesli w Twojej okolicy jest dostepna informacja o pomiarach wodowskazu
-na rzece możesz sprawdzic to na
+na rzece możesz sprawdzic to na mapie
 
+https://hydro.imgw.pl
+
+Wodowskazy sa zaznaczone jako trójkąty
 
 Otwierajac informacje na mapie o wodowskazie obok nazwy w nawiasie bedzie informacje o ID 
 wodowkazu np 
 
  SOBIANOWICE (151220100)
 
-Musisz spisac tez wartosci "Stan alarmowy" oraz "Stan ostrzegwaczy" 
-Dane te wpiszesz w hydro.pl 
+Musisz spisac ten ID  w pliku hydro.pl dla jednego wodowskazu:
+
+my @idstn=(153180090);
+
+kiedy więcej niż jeden wodowskaz użyj takiego zapisu:
+
+my @idstn=(153180090,153180080);
+
 
 Zrob kopie pliku hydro-cron do katalogu /etc/cron.d/
 
-Zrob restart crontab
+Plik ten bedzie cyklicznie pobieral dane co 1 godzine miedzy 5 a 23
 
-/etc/init.d/cron restart
-
-Dane dla svxlink bedą umieszczane w pliku hydro.tcl w katalogu /var/spool/bulletins/
-
-Patrz opis /opt/fmpoland/wx-info/ w pliku readme.txt 
-
+Uzyj kodu DTMF 64#  aby odtworzyc tresc komunikatu
