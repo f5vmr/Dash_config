2b74a6a4cc784081a5d082585e66f51a55d5d2b0
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-01-28 16:26:23 +0000

    m

--- opt/fmpoland/audiotest/audio.sh
+++ opt/fmpoland/audiotest/audio.sh
@@ -8,13 +8,13 @@ then
 rm /var/www/html/audio/*.wav
 fi 
 echo ""
-echo "Nagrywanie audio 10 sek, aby zatrzymać nagrywanie przed 10 sekundami użyj klawiszy CTRL+C"
+echo "Audio Recording for 10 seconds. To halt sooner type Cntrl-C."
 echo " "
 arecord -D hw:Loopback,1,4 -V mono -r 48000 -f S16_LE -c1 -d 15 /var/www/html/audio/audio-$(date +%Y-%m-%d-%H-%M-%S).wav
 echo ""
 MYIP=$(hostname -I | awk '{print $1}')
 echo " "
-echo " Możesz teraz odsłuchać swoje audio na stronie svxlink dashboard:"
+echo " You can now listen to your audio on the svxlink dashboard:"
 echo ""
 echo "                   http://$MYIP/audio"
 echo ""
